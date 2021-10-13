af4010df14a42adb6b19ec7e5a6158a1929a656e
SeC00y <51759097+SeC00y@users.noreply.github.com>
2021-10-13 17:49:32 -0300

    arreglo insert usuario

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -2,7 +2,7 @@
 
 include ("functions/setup.php");
 
-$sql="INSERT INTO usuario SET rut='".$_POST['frut']."',nombre='".$_POST['fnombre']."',appaterno='".$_POST['fappaterno']."',apmaterno='".$_POST['fapmaterno']."',usuario='".$_POST['fusuario']."',clave='".md5($_POST['fclave'])."',cargo='".$_POST['fcargo']."',foto='foto',estado='".$_POST['festado']."'";
+$sql="INSERT INTO usuario SET rut='".$_POST['frut']."',nombre='".$_POST['fnombre']."',appaterno='".$_POST['fappaterno']."',apmaterno='".$_POST['fapmaterno']."',usuario='".$_POST['fusuario']."',clave='".md5($_POST['fclave'])."',cargo='".$_POST['fcargo']."',estado='".$_POST['festado']."'";
 mysqli_query(conectar(),$sql);
 
 header('Location:formulario.php');
