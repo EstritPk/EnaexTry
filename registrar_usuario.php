b28d346a09abeb56940aeed3af31671531a16c5a
EstritPk <street.rojas69@gmail.com>
2021-10-13 22:35:19 -0300

    registro de usuario aiuda

--- registrar_usuario.php
+++ registrar_usuario.php
@@ -3,8 +3,8 @@
 include ("functions/setup.php");
 //$con=mysqli_connect("localhost","root","root","enaexbom");
 
-$sql="INSERT INTO usuario SET rut='".$_POST['frut']."',nombre='".$_POST['fnombre']."',appaterno='".$_POST['fappaterno']."',apmaterno='".$_POST['fapmaterno']."',clave='".md5($_POST['fclave'])."',
-usuario='".$_POST['fusuario']."',cargo='".$_POST['fcargo']."',estado='".$_POST['festado']."' ";
+$sql="INSERT INTO usuario SET rut='".$_POST['frut']."', nombre='".$_POST['fnombre']."', appaterno='".$_POST['fappaterno']."', apmaterno='".$_POST['fapmaterno']."',
+clave='".md5($_POST['fclave'])."', usuario='".$_POST['fusuario']."', estado='".$_POST['festado']."', cargo='".$_POST['fcargo']."' ";
 mysqli_query(conectar(),$sql);
 
 header('Location:formulario.php');
