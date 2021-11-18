0c0277f3215c49b44c1b8dbf48e47a6d9ee66746
EstritPk <street.rojas69@gmail.com>
2021-11-18 01:34:14 -0300

    no puedo mah seniores

--- registrar_sp.php
+++ registrar_sp.php
@@ -1,8 +1,8 @@
 <?php
 include ("functions/setup.php");
 
-if(isset($_GET['sppro']))
-{
+if(isset($_GET['sppro'])){
+
     $sql="DELETE FROM solicitud_pro  WHERE Id=".$_GET['sppro'];
         mysqli_query(conectar(),$sql);
         header('Location:crear_sp.php');
@@ -27,18 +27,27 @@ switch($op){
         break;
 }
 function ingresar()
-{                                            
-    $sql="INSERT INTO solicitud_pro SET rut_usu='".$_POST['frut']."',nombre_usu='".$_POST['fnombre']."',codigo_prod='".$_POST['sop']."',cantidad='".$_POST['fcantidad']."',estado=0,fecha_sp='".fechabd(fechahoy())."',descripcion='".$_post['des_sp']."'";
+{                  
+  //$sql="INSERT INTO solicitud_pro SET rut_usu='".$_POST['frut']."',nombre_usu='".$_POST['fnombre']."',codigo_prod='".$_POST['sop']."',cantidad='".$_POST['fcantidad']."',estado=0,fecha_sp='".fechabd(fechahoy())."',descripcion='".$_post['des_sp']."'";         
+    $sql="INSERT INTO solicitud_pro SET rut_usu='".$_POST['frut']."',nombre_usu='".$_POST['fnombre']."',codigo_prod='".$_POST['sop']."',cantidad='".$_POST['fcantidad']."',estado=1,fecha_sp='".fechabd(fechahoy())."',descripcion='".$_post['des_sp']."' ";
     mysqli_query(conectar(),$sql);
     
     
     
     header('Location:crear_sp.php');
 
+    if(isset($_GET['eliusu']))
+{
+    $sql="DELETE FROM solicitud_pro  WHERE Id=".$_POST['idoc'];
+        mysqli_query(conectar(),$sql);
+        header('Location:crear_sp.php');
+
+}
+
 }
 function modificar()
 {
-    $sql="UPDATE   solicitud_pro SET nombre_usu='".$_POST['fnombre']."',codigo_prod='".$_POST['sop']."',cantidad='".$_POST['fcantidad']."',estado=0,fecha_sp='".fechabd(fechahoy())."',descripcion='".$_post['des_sp']."'  WHERE Id=".$_POST['idoc'];
+    $sql="UPDATE   solicitud_pro SET codigo_prod='".$_POST['cod_pro']."',cantidad='".$_POST['fcantidad']."',estado=0,fecha_sp='".fechabd(fechahoy())."',descripcion='".$_post['des_sp']."'  WHERE Id=".$_POST['idoc'];
 mysqli_query(conectar(),$sql);
 
 header('Location:crear_sp.php');
@@ -68,4 +77,4 @@ function cancelar()
 
 
 
-?>
+
